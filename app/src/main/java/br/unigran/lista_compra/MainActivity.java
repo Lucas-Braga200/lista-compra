663dee6f97736092bd97b3ec3d79ed42ab8395a6
Lucas Braga <802.266@alunos.unigran.br>
2022-08-24 21:46:31 -0400

    Implementa listagem

--- app/src/main/java/br/unigran/lista_compra/MainActivity.java
+++ app/src/main/java/br/unigran/lista_compra/MainActivity.java
@@ -3,13 +3,24 @@ package br.unigran.lista_compra;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     EditText nome;
     EditText marca;
     EditText quantidade;
+
+    List<Produto> dados;
+
+    ListView listagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +30,22 @@ public class MainActivity extends AppCompatActivity {
         nome = findViewById(R.id.nome);
         marca = findViewById(R.id.marca);
         quantidade = findViewById(R.id.quantidade);
+
+        dados = new ArrayList();
+
+        listagem = findViewById(R.id.lista);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, dados);
+        listagem.setAdapter(adapter);
+    }
+
+    public void salvar(View view) {
+        Produto produto = new Produto();
+        produto.setMarca(marca.getText().toString());
+        produto.setNome(nome.getText().toString());
+        produto.setQuantidade(Integer.parseInt(quantidade.getText().toString()));
+
+        dados.add(produto);
+        Toast.makeText(this, "Produto salvo com sucesso.", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/br/unigran/lista_compra/Produto.java
@@ -0,0 +1,40 @@
+package br.unigran.lista_compra;
+
+public class Produto {
+    private String nome;
+    private String marca;
+    private Integer quantidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", marca='" + marca + '\'' +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+}
